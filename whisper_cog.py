c96a0d3bdd7c0e5e33a6b5e439b5d80ccd4b6969
Speacky081 <agrabowatschki@gmail.com>
2025-08-16 20:24:18 +0200

    added ability to reply and attach attachments to messages

--- whisper_cog.py
+++ whisper_cog.py
@@ -19,22 +19,46 @@ class WhisperCog(dc.ext.commands.Cog):
     async def on_message(self, message: dc.Message):
         '''Listen for non-whisper messages and delete them'''
         if not message.author.bot and message.channel.id == 1397636825971032095:
+            if len(message.attachments[0]) > 0:
+                attachment_url = message.attachments[0].url
+                await message.channel.send(attachment_url)
+            await message.channel.send(message.content)
             await message.delete()
+
+    @app_commands.context_menu(name="Antworten")
+    async def reply(self, interaction: dc.Interaction, message: dc.Message, reply: str):
+        """Enable people to reply-whisper to messages"""
+        await interaction.response.defer(ephemeral=True)
+        channel = self.bot.get_channel(1397636825971032095)
+        if channel is None:
+            channel = await self.bot.fetch_channel(1397636825971032095)
+        if message.channel.id != 1397636825971032095:
+            await interaction.followup.send("Du kannst das nur im anonymous channel machen", ephemeral=True)
+            return
+        await message.reply(" \n" + "**ANONYM  **" + reply, allowed_mentions = dc.AllowedMentions(everyone=False))
 
     @app_commands.command(
         name="whisper", description="Sag etwas, aber bleib dabei anonym")
-    async def whisper(self, interaction: dc.Interaction, message: str):
+    async def whisper(self, interaction: dc.Interaction, message: str, att: dc.Attachment = None):
         '''so simple it doesn't need a docstring'''
         await interaction.response.defer(ephemeral=True)
         channel = self.bot.get_channel(1397636825971032095)
         if channel is None:
             channel = await self.bot.fetch_channel(1397636825971032095)
-        await channel.send(" \n" + "**ANONYM  **" + message, allowed_mentions = dc.AllowedMentions(everyone=False))
+        if att is None:
+            await channel.send(" \n" + "**ANONYM  **" + message, allowed_mentions = dc.AllowedMentions(everyone=False))
+        else:
+            file = await att.to_file()
+            await channel.send(" \n" + "**ANONYM  **" + message, file=file, allowed_mentions=dc.AllowedMentions(everyone=False))
         with open("whisper_clients.txt", "r", encoding="utf-8") as clients:
             self.clients = [line.strip() for line in clients]
         for client in self.clients:
             channel = await self.bot.fetch_user(int(client))
-            await channel.send(" \n" + "**ANONYM  **" + message, allowed_mentions = dc.AllowedMentions(everyone=False))
+            if att is None:
+                await channel.send(" \n" + "**ANONYM  **" + message, allowed_mentions = dc.AllowedMentions(everyone=False))
+            else:
+                file = await att.to_file()
+                await channel.send(" \n" + "**ANONYM  **" + message, file=file, allowed_mentions = dc.AllowedMentions(everyone=False))
         await interaction.delete_original_response()
 
     @app_commands.command(
